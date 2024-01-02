b61a9f437c0e0ff584c38a1c6d03ec277df10f51
fofosharawi <yazansharawi25@gmail.com>
2024-01-02 20:55:29 +0300

    first push

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <v-checkbox label="Checkbox"></v-checkbox>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    files: [],
+  }),
+};
+</script>
